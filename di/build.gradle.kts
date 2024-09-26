8b336cd213f5f2a1b3a1bc3cbfaa11b8385f1595
Sotiris Sapakos <98215854+SotirisSapak@users.noreply.github.com>
2024-09-26 10:33:51 +0300

    Setting up core modules
    Create foundation modules:
    * core
    * framework
    * di (dependencies)

--- di/build.gradle.kts
+++ di/build.gradle.kts
@@ -1,20 +1,17 @@
 plugins {
-    alias(libs.plugins.android.application)
+    alias(libs.plugins.android.library)
     alias(libs.plugins.jetbrains.kotlin.android)
 }
 
 android {
-    namespace = "com.sotirisapak.apps.pokemonexplorer"
+    namespace = "com.sotirisapak.libs.pokemonexplorer.di"
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.sotirisapak.apps.pokemonexplorer"
         minSdk = 28
-        targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
     }
 
     buildTypes {
@@ -40,13 +37,6 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
-    implementation(libs.androidx.activity)
-    implementation(libs.androidx.constraintlayout)
-
-    implementation(project(":core"))
-    implementation(project(":framework"))
-    implementation(project(":di"))
-
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -21,4 +21,6 @@ dependencyResolutionManagement {
 
 rootProject.name = "Pokemon Explorer App"
 include(":app")
- 
+include(":core")
+include(":framework")
+include(":di")
